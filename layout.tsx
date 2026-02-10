6e8e3df5d2fb6fdce0a6f13a92075c26bd36deef
giovannicatapane <gicocatapane@gmail.com>
2026-02-10 13:52:24 -0300

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,14 +1,1 @@
-import "./globals.css";
-import React from "react";
 
-export const metadata = { title: "Logquim Manutenção", description: "OS + Estoque + Produtividade" };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="pt-br">
-      <body>
-        <div className="container">{children}</div>
-      </body>
-    </html>
-  );
-}
